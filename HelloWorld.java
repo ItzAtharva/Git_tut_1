d493a1877cc4dab1d1a86787a66d9c84d88be9b9
itzAtharva <atharvajagtap000@gmail.com>
2022-03-22 17:37:57 +0530

    Hello

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(""); 
+    }
+    public void display()
+    {
+        System.out.println("this is first program");
+    }
+}
